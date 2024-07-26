c8bfc7e06f1b867eeed9998cb31259f0076ea2c6
Youssef Alboraei <ygalboraei@gmail.com>
2024-07-26 04:32:16 +0100

    Complete adaption
    Still testing how for it will go

---
+++ onpolicy/scripts/train_swarm_scripts/train_swarm_single.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+env="SwarmEnv"
+scenario="single_transport" 
+num_agents=15
+algo="rmappo" #"mappo" "ippo"
+exp="check"
+seed_max=1
+
+echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
+for seed in `seq ${seed_max}`;
+do
+    echo "seed is ${seed}:"
+    CUDA_VISIBLE_DEVICES=0 python ../train/train_swarm.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+    --scenario_name ${scenario} --num_agents ${num_agents} --seed ${seed} \
+    --n_training_threads 1 --n_rollout_threads 2 --num_mini_batch 1 --episode_length 15000 --num_env_steps 20000000 \
+    --ppo_epoch 10 --use_ReLU --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" --user_name "ygalboraei-university-of-bristol"
+done
